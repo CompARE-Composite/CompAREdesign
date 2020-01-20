ff540f2c26b14e90844af0f8fb0f23aec5d2843b
raquelrovira96 <58967077+raquelrovira96@users.noreply.github.com>
2020-01-20 19:31:58 +0100

    Update effect_ce.R

--- CompARE-bin/R/effect_ce.R
+++ CompARE-bin/R/effect_ce.R
@@ -22,10 +22,6 @@
 #' For defect, if you don't specify the type of effect you want to obtain,
 #' it calculates the difference in proportions.
 #' 
-#' @example effect_ce(0.4,0.3,0.5,0.6,0.32)
-#' @example effect_ce(0.5,0.3,0.654,0.214,0.3,"diff")
-#' @example effect_ce(0.5,0.34,0.65,0.23,0.4,"rr")
-#' @example effect_ce(0.54,0.23,0.65,0.76,-0.3,"or")
 effect_ce <- function(p0_e1, p0_e2, p1_e1, p1_e2, rho, effect_ce = "diff"){
   if(p0_e1 < 0 || p0_e1 > 1){
     stop("The probability of observing the event E1 (p_e1) must be number between 0 and 1")
@@ -35,7 +31,7 @@ effect_ce <- function(p0_e1, p0_e2, p1_e1, p1_e2, rho, effect_ce = "diff"){
     stop("The probability of observing the event E1 (p_e1) must be number between 0 and 1")
   }else if(p1_e2 < 0 || p1_e2 > 1){
     stop("The probability of observing the event E2 (p_e2) must be number between 0 and 1")
-  }else if(rho < max(c(lower_corr(p0_e1,p0_e2),lower_corr(p1_e1,p1_e2)))  ||  rho > max(c(upper_corr(p0_e1,p0_e2),upper_corr(p1_e1,p1_e2)))){
+  }else if(rho <= max(c(lower_corr(p0_e1,p0_e2),lower_corr(p1_e1,p1_e2)))  ||  rho >= max(c(upper_corr(p0_e1,p0_e2),upper_corr(p1_e1,p1_e2)))){
     stop("The correlations of events must be in the correct interval")
   }else if(effect_ce != "rr" && effect_ce != "diff" && effect_ce != "or"){
     stop("You have to choose between odds ratio, relative risk or difference in proportions")
