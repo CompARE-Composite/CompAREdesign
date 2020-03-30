07238ec04308841c9e0f5f8f7703ef4e9a888352
MBR <marta.bofill.roig@upc.edu>
2020-03-30 10:47:11 +0200

    Auto stash before merge of "master" and "origin/master"

--- CompARE-bin/tests/testthat/test-CBEval1.R
+++ CompARE-bin/tests/testthat/test-CBEval1.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 context("CBEval1")
 
 test_that("CBEval1 works", {
@@ -172,3 +173,210 @@ test_that("CBEval1 works", {
 
 }
 )
+=======
+context("CBEval1")
+
+test_that("CBEval1 works", {
+
+  ##BLOC A - Validacions per a la funció prob_ce
+
+  prob_ce_1 <- prob_ce(0.54,0.4,0.2)
+  prob_ce_2 <- prob_ce(0.4,0.54,0.2)
+  prob_ce_3 <- prob_ce(0.4,0.54,0.2)
+  prob_ce_4 <- prob_ce(0.6,0.3,0.5)
+
+  #A
+  expect_equal(prob_ce_1, prob_ce_2)
+  expect_equal(prob_ce_3, prob_ce_2)
+  expect_equal(prob_ce_1, prob_ce_3)
+  expect_false(prob_ce_1, prob_ce_4)
+
+  #B
+  expect_that(prob_ce_1, is_a("numeric"))
+  expect_that(prob_ce_2, is_a("numeric"))
+  expect_that(prob_ce_3, is_a("numeric"))
+  expect_that(prob_ce_4, is_a("numeric"))
+
+  #C
+  expect_that(length(prob_ce_1),is_identical_to(length(prob_ce_2)),is_identical_to(length(prob_ce_3)),is_identical_to(length(prob_ce_4)))
+
+  #D
+  expect_error(prob_ce())
+  expect_error(prob_ce(p_E1=0.3, p_E2=NULL, rho=NULL))
+  expect_error(prob_ce(p_E1=NULL, p_E2=0.3, rho=NULL))
+  expect_error(prob_ce(p_E1=NULL, p_E2=NULL, rho=0.3))
+
+  #E
+  expect_that(prob_ce(3,0.4,0.5), throws_error("p_e1 numeric value are not allowed"))
+  expect_that(prob_ce(0.3,4,0.5), throws_error("p_e2 numeric value are not allowed"))
+  expect_that(prob_ce(0.2,0.4,5), throws_error("rho numeric value are not allowed"))
+
+  #BLOC B - Validacions de les funcions que s'utilitzen per calcular correlacions
+
+  Lower_corr_1 <- lower_corr(0.2,0.3)
+  Lower_corr_2 <- lower_corr(0.5,0.5)   #individus amb 50% de probabilitat
+  Upper_corr_1 <- upper_corr(0.6,0.4)
+  Upper_corr_2 <- upper_corr(0.2,0.2)  #individus amb 20% de probabilitat
+  Upper_corr_3 <- upper_corr(0.2,0.3)
+
+  #A
+  expect_true(Upper_corr_2 == 1)
+  expect_true(Lower_corr_2 == -1)
+  expect_false(Lower_corr_1 == Upper_corr_3)
+
+  #B
+  expect_that(Lower_corr_1, is_a("numeric"))
+  expect_that(Lower_corr_2, is_a("numeric"))
+  expect_that(Upper_corr_1, is_a("numeric"))
+  expect_that(Upper_corr_2, is_a("numeric"))
+  expect_that(Upper_corr_3, is_a("numeric"))
+
+  #C
+  expect_that(length(Lower_corr_1),is_identical_to(length(Lower_corr_1)))
+  expect_that(length(Upper_corr_1),is_identical_to(length(Upper_corr_2)),is_identical_to(length(Upper_corr_3)))
+
+
+
+  #D
+  expect_error(Lower_corr())
+  expect_error(Lower_corr(p_E1=0.3, p_E2=NULL))
+  expect_error(Lower_corr(p_E1=NULL, p_E2=0.3))
+  expect_error(Upper_corr())
+  expect_error(Upper_corr(p_E1=0.3, p_E2=NULL))
+  expect_error(Upper_corr(p_E1=NULL, p_E2=0.3))
+
+  #E
+  expect_that(Lower_corr(4,0.3), throws_error("p.e1 numeric value are not allowed"))
+  expect_that(Lower_corr(0.4,3), throws_error("p.e2 numeric value are not allowed"))
+  expect_that(Upper_corr(0.4,3), throws_error("p.e2 numeric value are not allowed"))
+  expect_that(Upper_corr(4,0.3), throws_error("p.e1 numeric value are not allowed"))
+
+  # BLOC C - Validacio de la funció que calcula els efectes de CBE
+
+  eff_a = effect_ce(0.3,0.4,0.2,0.5,0.32, type = "diff")
+  eff_b = effect_ce(0.3,0.4,0.2,0.5,0.32)
+  eff_c = effect_ce(0.3,0.4,0.2,0.5,0.32, type = "or")
+  eff_d = effect_ce(0.65,0.23,0.42,0.15,0.32, type = "or")
+  eff_e = effect_ce(0.3,0.4,0.2,0.5,0.32, type = "rr")
+  eff_f = effect_ce(0.43,0.87,0.32,0.25,0.23, type = "rr")
+
+  #A
+  expect_true(((((length(a$Diff) == length(b$Diff)) == length(c$Diff)) == length(d$Diff))
+               == length(e$Diff)) == length(f$Diff))
+  #B
+  condition <- function(a,b,c){
+    i <- 3
+    if(c == "T"){
+      expect_true(a[i,] == b[i,])
+    }else if(c == "F"){
+      expect_false(a[i,] == b[i,])
+    }
+  }
+
+  condition(a,b,"T")
+  condition(a,c,"F")
+  condition(a,e,"F")
+  condition(e,c,"F")
+
+  condition(c,d,"F")
+  condition(e,f,"F")
+
+  #C
+  expect_error(effect_ce())
+  expect_error(effect_ce(p0_E1 = 0.3, p0_E2 = 0.4,p1_E1 = 0.2, p1_E2 = 0.5))
+  expect_error(effect_ce(p0_E1 = 0.3, p0_E2 = 0.4,p1_E1 = 0.2, rho = 0.5))
+  expect_error(effect_ce(p0_E1 = 0.3, p0_E2 = 0.4,p1_E2 = 0.2, rho = 0.5))
+  expect_error(effect_ce(p0_E1 = 0.3, p1_E1 = 0.4,p1_E2 = 0.2, rho = 0.5))
+  expect_error(effect_ce(p0_E2 = 0.3, p1_E1 = 0.4,p1_E2 = 0.2, rho = 0.5))
+
+  #D
+   expect_that(effect_ce(3,0.4,0.2,0.5,0.32), throws_error("p0_e1 numeric value are not allowed"))
+  expect_that(effect_ce(0.3,4,0.2,0.5,0.32), throws_error("p0_e2 numeric value are not allowed"))
+  expect_that(effect_ce(0.3,0.4,2,0.5,0.32), throws_error("p1_e1 numeric value are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,5,0.32), throws_error("p1_e2 numeric value are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,5,32), throws_error("p1_e2 numeric value are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,32), throws_error("rho numeric value are not allowed"))
+
+  #Saber com validar els caracters
+
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "Diff"), throws_error("This effect type are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "dIff"), throws_error("This effect type are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "diFf"), throws_error("This effect type are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "difF"), throws_error("This effect type are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "Or"), throws_error("This effect type are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "oR"), throws_error("This effect type are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "Rr"), throws_error("This effect type are not allowed"))
+  expect_that(effect_ce(0.3,0.4,0.2,0.5,0.32, type = "rR"), throws_error("This effect type are not allowed"))
+
+  #BLOC D - Validació de la grandària mostral
+
+
+  ss_a = sample_size_ce(0.2,0.3,0.1,0.2,"diff",0.4, 0.05,0.2,"unpooled Variance")
+  ss_b = sample_size_ce(0.2,0.3,0.1,0.2,rho = 0.4)
+  ss_c = sample_size_ce(0.2,0.3,0.1,0.2,rho = 0.4, unpooled = "unpooled Variance")
+
+  ss_d = sample_size_ce(0.1,0.2,0.2,0.3,"rr",0.4, 0.05,0.2,"unpooled Variance")
+  ss_e = sample_size_ce(0.2,0.3,0.1,0.2,"rr",0.4, 0.05,0.2,"Variance")
+
+  ss_f = sample_size_ce(0.1,0.2,0.2,0.3,"or",0.4, 0.05,0.2,"unpooled Variance")
+  ss_g = sample_size_ce(0.2,0.3,0.1,0.2,"or",0.4, 0.05,0.2,"unpooled Variance")
+  ss_h = sample_size_ce(0.2,0.3,0.1,0.2,"or",0.4, 0.05,0.2,"Variance")
+
+
+  validation <- function(a,b,c){
+    i <- 3
+    if(c == "T"){
+      expect_true(a[i,] == b[i,])
+    }else if(c == "F"){
+      expect_false(a[i,] == b[i,])
+    }
+  }
+
+  validation(ss_a,s_b,"T")
+  validation(ss_a,s_c,"T")
+  validation(ss_c,s_b,"T")
+
+  validation(ss_d,ss_e,"F")
+  validation(ss_f,ss_g,"T")
+  validation(ss_f,ss_h, "F")
+
+  limits <- function(parm, inf, sup){
+    if(parm == "alpha"){
+      for (i in seq(inf,sup, 0.01)) {
+        expect_true(sample_size_ce(0.2,0.3,0.1,0.2,"diff",0.4, i,2,"unpooled Variance"))
+      }
+    }else if(parm == "beta"){
+      for (i in seq(inf,sup, 0.01)) {
+        expect_true(sample_size_ce(0.2,0.3,0.1,0.2,"diff",0.4, 0.05,i,"unpooled Variance"))
+      }
+    }
+  }
+
+  limits(alpha,0,1)
+  limits(beta,0,1)
+
+
+  expect_error(sample_size_ce())
+  expect_error(sample_size_ce(p0_E1 = 0.3, p0_E2 = 0.4,p1_E1 = 0.2, p1_E2 = 0.5))
+  expect_error(sample_size_ce(p0_E1 = 0.3, p0_E2 = 0.4,p1_E1 = 0.2, rho = 0.5))
+  expect_error(sample_size_ce(p0_E1 = 0.3, p0_E2 = 0.4,p1_E2 = 0.2, rho = 0.5))
+  expect_error(sample_size_ce(p0_E1 = 0.3, p1_E1 = 0.4,p1_E2 = 0.2, rho = 0.5))
+  expect_error(sample_size_ce(p0_E2 = 0.3, p1_E1 = 0.4,p1_E2 = 0.2, rho = 0.5))
+
+
+
+  expect_that(sample_size_ce(0.2,0.3,0.1,0.2,"diff",0.4, 0.05,2,"unpooled Variance"), throws_error("beta numeric value are not allowed"))
+  expect_that(sample_size_ce(0.2,0.3,0.1,0.2,"diff",0.4, 5,0.2,"unpooled Variance"), throws_error("alpha numeric value are not allowed"))
+  expect_that(sample_size_ce(0.2,0.3,0.1,0.2,"diff",4, 0.05,0.2,"unpooled Variance"), throws_error("rho numeric value are not allowed"))
+  expect_that(sample_size_ce(0.2,0.3,0.1,2,"diff",0.4, 0.05,0.2,"unpooled Variance"), throws_error("p1.e2 numeric value are not allowed"))
+  expect_that(sample_size_ce(0.2,0.3,1,0.2,"diff",0.4, 0.05,0.2,"unpooled Variance"), throws_error("p1.e1 numeric value are not allowed"))
+  expect_that(sample_size_ce(0.2,3,0.1,0.2,"diff",0.4, 0.05,0.2,"unpooled Variance"), throws_error("p0.e1 numeric value are not allowed"))
+  expect_that(sample_size_ce(2,0.3,0.1,0.2,"diff",0.4, 0.05,0.2,"unpooled Variance"), throws_error("p0.e2 numeric value are not allowed"))
+
+
+
+
+}
+  )
+
+>>>>>>> Stashed changes
