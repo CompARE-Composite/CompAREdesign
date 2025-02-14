5867288902d31d81c5c869bf2b9a6c905a70d706
Jordi Cortes <jordi.cortes-martinez@upc.edu>
2025-02-14 11:54:18 +0100

    typo plot_tte 4

--- R/plot_tte.R
+++ R/plot_tte.R
@@ -106,8 +106,7 @@ plot_tte <- function(p0_e1, p0_e2, HR_e1, HR_e2, beta_e1=1, beta_e2=1, case,
 
   if(summary == TRUE){
 
-    return(ggarrange(plot_surv, plot_effect, plot_ARE, plot_ss,
-                    ncol = 2, nrow = 2))
+    return(ggarrange(plot_surv, plot_effect, plot_ARE, plot_ss, ncol = 2, nrow = 2))
 
   } else if(type=='survival' & summary == FALSE){
 
